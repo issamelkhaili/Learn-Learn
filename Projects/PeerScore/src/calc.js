82e6dcc91a4e4cff350c104217b2df14b71bc662
Issam El Khaili <isel-kha@e2r13p1.1337.ma>
2025-02-18 21:20:44 +0100

    trash

---
+++ Projects/PeerScore/src/calc.js
@@ -0,0 +1,23 @@
+let	needed = 120;
+let	lefts;
+let	day;
+let today;
+let	date = new Date().toISOString().split("T");
+function	left(done)
+{
+	lefts = needed - done;
+	console.log(lefts);
+}
+
+function	getdaysleft(date)
+{
+	today = date.split('-')[2];
+	console.log(today);
+}
+
+getdaysleft("2025-02-29");
+function	daily(timeleft)
+{
+	day = timeleft/
+}
+function	
